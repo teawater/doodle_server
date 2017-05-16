fa5aa205f2f5aa2d68ad013763bd60c7e704ab7a
Hui Zhu <zhuhui@xiaomi.com>
2017-05-16 17:55:16 +0800

    Update

---
+++ design.mm
@@ -0,0 +1,27 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1494828043061" ID="ID_1437696010" MODIFIED="1494828512936" TEXT="&#x6838;&#x5fc3;&#x8fdb;&#x7a0b;&#x542f;&#x52a8;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1494828396731" ID="ID_966943359" MODIFIED="1494828659064" POSITION="right" STYLE="bubble" TEXT="&#x7ba1;&#x9053; new_con">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1494828060459" ID="ID_411263258" MODIFIED="1494828632384" POSITION="right" STYLE="bubble" TEXT="go &#x51fd;&#x6570;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1494828364682" ID="ID_317002171" MODIFIED="1494828718551" TEXT="&#x4ece; new_con &#x8bfb;&#x53d6; ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1494828098826" ID="ID_1345779729" MODIFIED="1494828667792" POSITION="right" STYLE="bubble" TEXT="websocket.Handler(onConnected)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1494828115610" ID="ID_1461431351" MODIFIED="1494828673032" POSITION="right" STYLE="bubble" TEXT="ListenAndServeTLS">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
