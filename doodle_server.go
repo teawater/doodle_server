078fc934787ae0acfacb78a5f2a550d85ae13e70
teawater <teawater@126.com>
2017-07-17 23:03:54 +0800

    Change to use other ws lib

--- doodle_server.go
+++ doodle_server.go
@@ -4,11 +4,10 @@ import (
 	"log"
 	"net/http"
 	"sync"
-	"time"
 	"fmt"
 	"container/list"
 
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
 )
 
 const ssl_crt = "server.crt"
@@ -28,7 +27,7 @@ var color_lock sync.RWMutex
 var clients *list.List
 var clients_lock sync.Mutex
 
-func sync_color_to_client(ws *websocket.Conn, id *uint64) (err error) {
+func sync_color_to_client(c *websocket.Conn, id *uint64) (err error) {
 	err = nil
 
 	color_lock.RLock()
@@ -44,16 +43,18 @@ func sync_color_to_client(ws *websocket.Conn, id *uint64) (err error) {
 
 	*id = color.Id
 	
-	err = websocket.JSON.Send(ws, color)
+	err = c.WriteJSON(color)
 
 	return
 }
 
-func handle_receive_pack(ws *websocket.Conn, quit chan bool) {
+func handle_receive_pack(c *websocket.Conn, quit chan bool) {
 	
 }
 
-func onConnected(ws *websocket.Conn) {
+var upgrader = websocket.Upgrader{} // use default options
+
+func onConnected(w http.ResponseWriter, r *http.Request) {
 	var err error
 	type reply_s struct {
 		fmt int
@@ -61,12 +62,19 @@ func onConnected(ws *websocket.Conn) {
 	}
 	var reply reply_s
 
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
+	defer c.Close()
+
 	//check the client
-	log.Println("Client:", ws.RemoteAddr(), ws.RemoteAddr().Network(), ws.RemoteAddr().String())
+	//log.Println("Client:", ws.RemoteAddr(), ws.RemoteAddr().Network(), ws.RemoteAddr().String())
+	log.Println("Client:", r);
 
 	//Handle first pack
-	ws.SetReadDeadline(time.Now().Add(time.Second * 60))
-	err = websocket.JSON.Receive(ws, &reply)
+	err = c.ReadJSON(&reply)
 	if err != nil {
 		log.Println("Get first packet error:", err)
 		return
@@ -78,7 +86,7 @@ func onConnected(ws *websocket.Conn) {
 
 	//First sync the color to the client
 	id := reply.id
-	err = sync_color_to_client(ws, &id)
+	err = sync_color_to_client(c, &id)
 	if err != nil {
 		log.Println("First sync fail:", err)
 		return
@@ -92,13 +100,13 @@ func onConnected(ws *websocket.Conn) {
 
 	/* Creat new goroutine to receive value.  */
 	quit_ch := make(chan bool, 1)
-	go handle_receive_pack(ws, quit_ch)
+	go handle_receive_pack(c, quit_ch)
 
 	loop := true
 	for loop {
 		select {
 			case <-sync_ch:
-				err = sync_color_to_client(ws, &id)
+				err = sync_color_to_client(c, &id)
 				if err != nil {
 					log.Println("Sync fail:", err)
 					loop = false
@@ -107,8 +115,6 @@ func onConnected(ws *websocket.Conn) {
 				loop = false
 		}
 	}
-
-	ws.Close()
 }
 
 func main() {
@@ -120,7 +126,7 @@ func main() {
 	}
 	clients = list.New()
 
-	http.Handle("/", websocket.Handler(onConnected))
+	http.HandleFunc("/", onConnected)
 
 	log.Fatal(http.ListenAndServeTLS(":443", ssl_crt, ssl_key, nil))
 }
